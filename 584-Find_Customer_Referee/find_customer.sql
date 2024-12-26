39a775dd8383f3c3ce81d4722145666da518d593
Sadam-Barkat <sadambarkat405@gmail.com>
2024-12-26 23:55:58 +0500

    Add Files

---
+++ 584-Find_Customer_Referee/find_customer.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+      name
+      FROM Customer
+      WHERE referee_id IS NULL
+
+UNION ALL
+
+SELECT
+      name
+      FROM Customer
+      WHERE referee_id != 2;      
+
+      
